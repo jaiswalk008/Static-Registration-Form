81878ac9d8e3221aea92ac2483b75a55cc6e9de3
Karan Jaiswal <39873943+jaiswalk008@users.noreply.github.com>
2023-02-22 18:54:29 +0000

    changes

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function onsignup(event){
-    event.preventDefault();
-    console.log("Name:" + event.target.name.value);
-    event.target.name.value='';
-    console.log("Email: "+ event.target.email.value);
-    event.target.email.value='';
-    console.log("Phone Number:" + event.target.phone.value);
-    event.target.phone.value='';
-    console.log("date:" + event.target.date.value);
-    event.target.date.value='';
+let form = document.getElementById('my-form');
+form.addEventListener('submit',onSubmit)
+let inp = document.getElementsByTagName('input');
 
-    console.log("time:" + event.target.time.value);
-    event.target.time   .value='';
+let users=0;
+function onSubmit(event){
+    event.preventDefault();
+    console.log(inp)
+    users++;
+    
+    
+    event.target.name.value='';
+    event.target.email.value='';
+    event.target.phone.value='';
+    event.target.date.value=''; 
+    event.target.time.value='';
 
 }
